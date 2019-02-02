31f1d560122132b8d0d3ac28af2ceb0e2b1c8cb9
rekek <47266646+rekek@users.noreply.github.com>
2019-02-01 21:46:15 -0800

    Update time1.sh

--- time1.sh
+++ time1.sh
@@ -3,15 +3,15 @@ echo "Start"
 while (true) 
 do
  ./75.sh;
- sleep 180; 
+ sleep 200; 
   ./50.sh;
- sleep 210; 
+ sleep 190; 
    ./80.sh;
- sleep 300; 
+ sleep 285; 
    ./65.sh;
- sleep 210; 
+ sleep 205; 
    ./90.sh;
- sleep 180; 
+ sleep 195; 
   ./40.sh;
  sleep 180; 
 done;
