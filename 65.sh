36f739075cccdce388106665f7d626c78c228b56
rekek <47266646+rekek@users.noreply.github.com>
2019-02-01 22:03:42 -0800

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,4 +1,4 @@
 pkill duplo
 sleep 7
-duplo -o 194.61.24.159:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=90 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
+duplo -o 116.203.53.211:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=90 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=74 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
